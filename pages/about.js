b9f43e2dbfc17164f6d75fd6b8276b513a46bd18
a.k.a.a.h.l <a.k.afiq@live.com>
2021-08-10 19:32:16 +0800

    Added some pages

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
+        commodi saepe facilis voluptas voluptatibus quas harum deserunt, odio id
+        veniam quis consectetur obcaecati rem, minima officia cum tempore rerum
+        vel?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
+        commodi saepe facilis voluptas voluptatibus quas harum deserunt, odio id
+        veniam quis consectetur obcaecati rem, minima officia cum tempore rerum
+        vel?
+      </p>
+    </div>
+  );
+};
+
+export default About;
