c204f93190fe58afa2c6cb81dd3fa40afee8f332
a.k.a.a.h.l <a.k.afiq@live.com>
2021-08-10 20:21:41 +0800

    Added Layout component and wrap Navbar and Footer around the children

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 import Footer from "../comps/Footer";
 import Navbar from "../comps/Navbar";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, saepe,
@@ -19,7 +19,10 @@ export default function Home() {
         vel, temporibus eum ex tempore ducimus rem unde perspiciatis ut iure
         magnam quo at omnis odio quas et architecto. Aliquid.
       </p>
-      <Footer />
+
+      <Link href="/ninjas">
+        <a>See ninja listing</a>
+      </Link>
     </div>
   );
 }
