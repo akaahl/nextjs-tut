c45ba30cca6efcd614af7a80a16b21ef92e98255
a.k.a.a.h.l <a.k.afiq@live.com>
2021-08-10 20:07:43 +0800

    Added Navbar and Footer

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Footer from "../comps/Footer";
+import Navbar from "../comps/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, saepe,
@@ -16,6 +19,7 @@ export default function Home() {
         vel, temporibus eum ex tempore ducimus rem unde perspiciatis ut iure
         magnam quo at omnis odio quas et architecto. Aliquid.
       </p>
+      <Footer />
     </div>
   );
 }
